0f084757df89494b22fa652871ec0109e15d2ace
Aura Herrera <aursolis@amazon.com>
2018-11-16 18:32:16 +0000

    Adding Dev

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World',
+        'output': 'Hello World - I'm DEV',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
