861a62fb73db9515ea6878c6158135497d294ddd
aursolis <45105343+aursolis@users.noreply.github.com>
2019-02-26 10:58:05 +0000

    sunny

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello Amazing Day!!!',
+        'output': 'Hello Sunny Day!!!',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
